abc5d700ab1a1ad7417fb88d3778530774dbfb34
Sourav Rakshit <rakshitsourav3@gmail.com>
2026-05-17 15:28:31 +0530

    feat: restore C++ skeleton templates under ~/.config/cpp
    Move the old vim/.vim/templates/CPP skeletons and compile_flags.txt
    into a modern ~/.config/cpp/ path managed by chezmoi.
    
    - skeleton.cc   – minimal C++ starter
    - skeleton.cpp  – full competitive-programming boilerplate
    - compile_flags.txt – clangd / coc.nvim compile flags

---
+++ private_dot_config/cpp/skeleton.cc
@@ -0,0 +1,15 @@
+/**
+ *  author: souravrax
+ *  created:
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define len(x) int((x).size())
+#define fast ios::sync_with_stdio(false), cin.tie(nullptr)
+using ll = long long;
+
+int32_t main() {
+    fast; 
+}
